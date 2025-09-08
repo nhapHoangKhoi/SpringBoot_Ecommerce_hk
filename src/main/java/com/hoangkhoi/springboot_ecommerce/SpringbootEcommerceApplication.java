f2347a90ae46ea2608078c3e69781300b2c80899
N H Khoi <nhaphk@gmail.com>
2025-09-08 10:35:04 +0700

    Create model Category, Product

--- src/main/java/com/hoangkhoi/springboot_ecommerce/SpringbootEcommerceApplication.java
+++ src/main/java/com/hoangkhoi/springboot_ecommerce/SpringbootEcommerceApplication.java
@@ -2,17 +2,9 @@ package com.hoangkhoi.springboot_ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class SpringbootEcommerceApplication {
-
-    @GetMapping("/hello")
-    String sayHello() {
-        return "Hello Spring Boot!";
-    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootEcommerceApplication.class, args);
---
+++ src/main/java/com/hoangkhoi/springboot_ecommerce/model/Product.java
@@ -0,0 +1,84 @@
+package com.hoangkhoi.springboot_ecommerce.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", length = 100, nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    @JsonBackReference
+    private Category category;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    //----- Start getter, setter -----//
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    //----- End getter, setter -----//
+}
